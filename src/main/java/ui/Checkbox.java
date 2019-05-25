dd8ccec6bf5b63ece6e139349af8c6313a585a49
Heikki Heiskanen <heikkiheiskanen@hotmail.com>
2019-05-25 22:17:38 +0300

    simple checkbox implementation

--- src/main/java/ui/Checkbox.java
+++ src/main/java/ui/Checkbox.java
@@ -1,28 +1,66 @@
 package ui;
 
+import core.Game;
+
 import java.awt.*;
 
 public class Checkbox extends GuiElement implements InteractableGuiElement {
 
     private boolean isSelected;
-    private Panel parent;
+    private String label;
+
+    private Font labelFont, checkboxFont;
+    private FontMetrics checkboxFontMetrics;
+
+    private final int checkboxHeight = 16, checkboxWidth = 16;
 
     public Checkbox(Panel parent, String label, boolean isSelected) {
         super(parent);
         this.isSelected = isSelected;
-        this.parent = parent;
+        this.label = label;
+
+        this.labelFont = Game.instance.getCustomFont().deriveFont(Font.PLAIN, 24);
+        this.checkboxFont = Game.instance.getCustomFont().deriveFont(Font.PLAIN, 24);
+
+        this.h = checkboxHeight;
+
+        // todo: hard coded value for single character width
+        this.w = checkboxWidth + label.length() * 10;
     }
 
     @Override
     public void render(Graphics g) {
 
+        Graphics2D g2d = (Graphics2D) g;
+
+        if(this.checkboxFontMetrics == null) {
+            this.checkboxFontMetrics = g.getFontMetrics(checkboxFont);
+        }
+
         // render box
-        g.drawRect();
+        g.setColor(Colors.BLACK);
+        g.fillRect(x, y, checkboxWidth, checkboxHeight);
 
         // render tick mark
+        if(isSelected) {
+            g.setColor(Colors.YELLOW);
+            g.setFont(checkboxFont);
+            g.drawString(
+                    "X",
+                    x + checkboxWidth / 2 - checkboxFontMetrics.stringWidth("X") / 2,
+                    y + checkboxHeight / 2 + checkboxFontMetrics.getHeight() / 2);
+        }
 
         // render label
-
+        if(label != null) {
+            g2d.setFont(labelFont);
+            g2d.setColor(Colors.BLACK);
+            g2d.drawString(
+                    label,
+                    x + margin * 2,
+                    y + checkboxHeight - 2 // todo: hard coded margin value
+            );
+        }
     }
 
     @Override
--- src/main/java/ui/GuiBuilder.java
+++ src/main/java/ui/GuiBuilder.java
@@ -5,6 +5,7 @@ import core.GuiActionCreator;
 public class GuiBuilder {
 
     public static Panel createLoginScreen() {
+
         VPanel panel =
                 GuiFactory.createDefaultCenteredPanel(
                         null, false, Colors.BLUE);
@@ -17,9 +18,12 @@ public class GuiBuilder {
 
         TextField usernameTextField = GuiFactory.createDefaultTextField(panel, "Username");
         TextField passwordTextField = GuiFactory.createDefaultPasswordField(panel, "Password");
+        Checkbox createNewAccountCheckbox = new Checkbox(
+                panel, "Create New Account", false);
 
         panel.addElement(usernameTextField);
         panel.addElement(passwordTextField);
+        panel.addElement(createNewAccountCheckbox);
 
         panel.addElement(GuiFactory.createDefaultConnectButton(
                 panel,
